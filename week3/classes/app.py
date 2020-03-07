d63145748ba6a10bbf1ba51ff83429ca5b30c48b
Allen <alleneben@gmail.com>
2020-03-07 08:52:03 -0500

    functions and classes

---
+++ week3/classes/app.py
@@ -0,0 +1,10 @@
+class App:
+    def __init__(self):
+        print('helloooooo')
+
+
+    def add(self,a,b):
+        return a + b
+    
+    def subt(self,a,b):
+        return a - b
---
+++ week3/init.py
@@ -0,0 +1,19 @@
+from classes.app import App
+
+
+
+app = App()
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ans = app.add(4,6)
+    sub = app.subt(8,7)
+    print(ans,sub)
